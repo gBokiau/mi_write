ea2021473ee4facb58586c086d176ab0591a8eda
AD7six <andydawson76@yahoo.co.uk>
2010-03-23 00:13:21 +0100

    need mi_enums too

--- config/control.php
+++ config/control.php
@@ -7,6 +7,6 @@ Description: A plugin providing core editing/publishing functionality
  This plugin provides the mechanism to edit entries/pages. It is intended to
  provide the core functionality for an app - not necessarily to be used by
  itself
-Depends: mi, mi_tags, mi_media, mi_settings
+Depends: mi, mi_enums, mi_tags, mi_media, mi_settings
 Recommends: mi_asset, mi_email, mi_enums, mi_media, mi_users
 Bugs: http://github.com/AD7six/mi_write/issues
