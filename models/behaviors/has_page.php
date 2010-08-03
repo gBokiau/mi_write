73d9ea5b8e56e05d705dfaa5f8f42417275b40e5
Guillaume Bokiau <guillaume.bokiau@gmail.com>
2010-08-03 16:52:48 +0200

    improving hasPage bahavior

--- models/behaviors/has_page.php
+++ models/behaviors/has_page.php
@@ -1,5 +1,13 @@
 <?php
 class HasPageBehavior extends ModelBehavior {
+	var $runtime= array();
+		function setup(&$Model, $settings) {
+			if (!isset($this->runtime[$Model->alias])) {
+				$this->runtime[$Model->alias] = array(
+					'display' => ''
+				);
+			}
+		}
 	function beforeFind(&$model, $query) {
 		$model->Behaviors->attach('Containable');
 		if ($model->findQueryType == 'list') { // && !isset($query['fields'])
@@ -9,13 +17,27 @@ class HasPageBehavior extends ModelBehavior {
 				$query['contain'] = array('Page'=>array('title'));
 			}
 		} elseif ($model->findQueryType == 'first') {
-			if(!isset($query['contain'])) {
-				$query['contain'] = array();
+			if (!isset($query['recursive']) || (isset($query['recursive']) && $query['recursive'] > -1)) {
+				$query['recursive'] = 1;
+				if(!isset($query['contain'])) {
+					$query['contain'] = array();
+				}
+				if(!array_key_exists('Page', $query['contain']))
+					$query['contain'][] = 'Page';
 			}
-			if(!array_key_exists('Page', $query['contain']))
-				$query['contain'] = $query['contain']+array('Page');
 		}
 		return $query;
+	}
+	
+	function afterSave(&$model, $created) {
+		if ($model->id && isset($model->data['Page'])) {
+			$this->runtime[$model->alias]['display'] = $model->data['Page']['title'];
+		}
+		
+	}
+	
+	function display(&$model) {
+		return $this->runtime[$model->alias]['display'];
 	}
 }
 ?>
